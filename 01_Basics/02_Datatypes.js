02910bcd86a0cf6f1b2deca7e9aab8adeac097a5
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-08-18 13:12:17 +0530

    Learned Dataypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,38 @@
+"use strict";  // Treats all the JavaScript Code a Newer Version
+
+// alert("HELLO!")  //Not Running in the terminal
+
+console.log(7
+    +               // Code Readability Should Be prioritized
+    7)
+
+
+
+// DataTypes
+
+let myNumber = 5;  // Number
+let myString = "Hello";  // String
+let myBoolean = true;  // Boolean
+let myNull = null;  // Null
+let myUndefined = undefined;  // Undefined
+            // or
+let my_defined;
+
+/*
+    number => 2 to power of 53
+    bigint => 
+    string => "" or ''
+    boolean => true or false
+    null => standalone value
+    undefined =>
+*/
+
+// Typeof
+
+console.log(typeof myNumber);  // Outputs: "number"
+console.log(typeof myString);  // Outputs: "string"
+console.log(typeof myBoolean);  // Outputs: "boolean"
+console.log(typeof myNull);  // Outputs: "object" (It's actually a special object)
+console.log(typeof myUndefined);  // Outputs: "undefined"
+console.log(typeof my_defined);   // Outputs: "undefined"
+console.log(typeof null);
