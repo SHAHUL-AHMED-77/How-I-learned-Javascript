76313b4cf2990fbe71925ef67be403684c3d26b4
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-08-19 22:50:55 +0530

    Learned Comparisions

---
+++ 01_Basics/05_Comparisions.js
@@ -0,0 +1,34 @@
+console.log("2">1)
+console.log("02">1)
+
+console.log("2"<1)
+console.log("02"<1)
+
+console.log("2"<=1)
+console.log("02"<=1)
+
+console.log("2">=1)
+console.log("02">=1)
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null <= 0)   //Comparisions Convert Null to Number , treat it as Zero
+console.log(null == 0)   //should be Ignored
+console.log(null >= 0)
+
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)   //should be Ignored
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+
+
+console.log(undefined >= undefined)
+
+
+// ===
+
+
+console.log("2"===2)              // Datatype Will be checked
