45790a2d67dd3021a7cebbc10252e37f84c4ef9d
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-08-21 23:20:21 +0530

    Strings_1

--- 01_Basics/07_strings.js
+++ 01_Basics/07_strings.js
@@ -2,7 +2,8 @@ const name = "Shahul"
 
 const Repo = 10
 
-// console.log(name + "Has" + Repo + "Repositories")      Old Method(Not recommended)
+// console.log(name + "Has" + Repo + "Repositories")      // Old Method(Not recommended)
 
+// Repo = 20    // Not Allowed
 
 console.log(`${name} has ${Repo} Repositories`)      // String interpolation
