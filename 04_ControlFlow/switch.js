502fdadbd33d32a3b2dd235ac36278ada2d6145b
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-09-04 22:04:42 +0530

    Learned Switch

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,23 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const mon = 1
+switch (mon) {
+    case 1:
+        console.log("Jan");
+        break;              // If break is not all conditions will execute except Default
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    default:
+        console.log("Default");
+        break;
+}
