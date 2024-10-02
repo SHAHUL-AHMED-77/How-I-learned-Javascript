03e881afd76e80a1eb1d76d063dd8147b3a0e5be
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-10-02 12:43:34 +0530

    Learned While And Do-While

---
+++ 05_Iterations/02_While.js
@@ -0,0 +1,31 @@
+// While :
+let a = 1;
+while(a <= 10) {
+    console.log(a);
+    a = a+1
+}
+
+const ar = ["a","b","c","d","e"]
+let arr = 0
+while (arr <ar.length) {
+    console.log(`${ar[arr]}`);
+    arr = arr + 1
+}
+
+//Do While :
+
+let s = 1
+
+do {
+    console.log(`${s}`);
+    s++
+    
+} while (s <= 10);
+
+let dr = ["Hello","Bye","World"]
+let d = 0
+do {
+    console.log(dr[d]);
+    d++
+    
+} while (d < dr.length);
